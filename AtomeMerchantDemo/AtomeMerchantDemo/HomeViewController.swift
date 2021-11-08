394f28b54ff17d7bcc5f073cd2b4b84b5958e5ff
boye.hou <boye.hou@advance.ai>
2021-11-08 15:26:05 +0800

    优化demo 实现
    优化demo 实现

---
+++ AtomeMerchantDemo/AtomeMerchantDemo/HomeViewController.swift
@@ -0,0 +1,18 @@
+//
+//  HomeViewController.swift
+//  AtomeMerchantDemo
+//
+//  Created by boye on 2021/11/8.
+//  Copyright © 2021 boye. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+}
